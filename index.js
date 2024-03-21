ef0d67a0a414014a3c287c7d31f2d180f26be758
SL-GW <162018972+SL-GW@users.noreply.github.com>
2024-03-21 18:06:39 +0100

    Injected the forecast into Javascript

--- index.js
+++ index.js
@@ -55,7 +55,34 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tues", "Wed", "Thurs", "Fri", "Sat"];
+
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="day-one">
+      <div class = "day-one-day">
+            ${day}</div>
+            <img
+              src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"
+              width="70"
+            />
+            <div class="day-one-temperature">
+              <span class="day-one-high">14º</span
+              ><span class="day-one-low">10º</span>
+            </div>`;
+  });
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Madrid");
+displayForecast();
